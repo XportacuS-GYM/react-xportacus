e15070f138a9565efb39c14fd3508773b6582328
CesarAstorgaOchoa <18170276@itculiacan.edu.mx>
2023-10-15 14:41:47 -0700

    conexion backend

--- src/views/Registro.jsx
+++ src/views/Registro.jsx
@@ -1,6 +1,32 @@
-
+import {Link} from 'react-router-dom'
+import {createRef, useState} from 'react'
+import clienteAxios from '../config/axios'
 
 export default function Registro() {
+
+    const nameRef = createRef();
+    const emailRef = createRef();
+    const passwordRef = createRef();
+    const passwordConfirmationRef = createRef();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+        const datos ={
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            password_confirmation : passwordConfirmationRef.current.value
+        }
+        try {
+            const respuesta = await clienteAxios.post('http://localhost/api/registro', datos);
+            console.log(respuesta);
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
     return (
         <div className="relative flex flex-col rounded-xl bg-transparent bg-clip-border text-gray-700 shadow-none">
             <h4 className="block font-sans text-2xl font-semibold leading-snug tracking-normal text-[#35414B]  antialiased">
@@ -9,12 +35,16 @@ export default function Registro() {
             <p className="mt-1 block font-sans text-base font-normal leading-relaxed text-gray-700 antialiased">
                 Crea tu cuenta para poder disfrustar tus beneficios.
             </p>
-            <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+            <form onSubmit={handleSubmit} className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
                 <div className="mb-4 flex flex-col gap-6">
                     <div className="relative h-11 w-full min-w-[200px]">
                         <input
+                            type="text"
+                            id="name"
+                            name="name"
                             className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-hardColor focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                            placeHolder=" "
+                            placeholder=''
+                            ref={nameRef}
                         />
                         <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-mainColor peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-mainColor peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-mainColor peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                             Nombre
@@ -22,8 +52,12 @@ export default function Registro() {
                     </div>
                     <div className="relative h-11 w-full min-w-[200px]">
                         <input
+                            type='email'
+                            id='email'
+                            name="email"
                             className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-hardColor focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                            placeHolder=" "
+                            placeholder=''
+                            ref={emailRef}
                         />
                         <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-mainColor peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-mainColor peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-mainColor peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                             Email
@@ -32,8 +66,11 @@ export default function Registro() {
                     <div className="relative h-11 w-full min-w-[200px]">
                         <input
                             type="password"
+                            id="password"
+                            name="password"
                             className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-hardColor focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                            placeHolder=" "
+                            placeholder=''
+                            ref={passwordRef}
                         />
                         <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-mainColor peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-mainColor peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-mainColor peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                             Contraseña
@@ -41,9 +78,12 @@ export default function Registro() {
                     </div>
                     <div className="relative h-11 w-full min-w-[200px]">
                         <input
-                            type="password_confirmation"
+                            type="password"
+                            id="password_confirmation"
+                            name="password_confirmation"
                             className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-hardColor focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                            placeHolder=" "
+                            placeholder=''
+                            ref={passwordConfirmationRef}
                         />
                         <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-mainColor peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-mainColor peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-mainColor peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                             Confirmar Contraseña
@@ -53,7 +93,7 @@ export default function Registro() {
                 
                 <button
                     className="mt-6 block w-full select-none rounded-lg bg-mainColor py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-hardColor/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                    type="button"
+                    type='submit'
                     data-ripple-light="true"
                 >
                     Registrarse
